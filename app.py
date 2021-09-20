78c378a19118b5c61b57790ed7fc4a6f0f45fa5e
dkasatchkow <d.kasatchkow@gmail.com>
2021-09-20 17:48:26 +0800

    Added basic feedback form

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/submit", methods=["GET","POST"])
+def user_feedback():
+    if request.method == "GET":
+        return "Nothing to see here"
+    if request.method == "POST":
+        info = request.form
+        print(info)
+        return f"Thanks"
 
 if __name__=="__main__":
     app.run(debug=True)
